07bbf27f5bacb585f0f28bd0f8ace66fde5f2b9a
Geuskens Ben <ben.geuskens@cegeka.com>
2022-12-05 22:27:28 +0100

    day2 part1

--- day2/day2.go
+++ day2/day2.go
@@ -1,14 +1,14 @@
 package day2
 
 import (
-	//"AdventOfCode2022/tools"
+	"AdventOfCode2022/tools"
 	"fmt"
 )
 
 func Part1() {
-	smallResult := CalulateScore(GetInput("C:\\Repos\\AdventOfCode2022\\day2\\day2_ex.txt"))
+	smallResult := CalulateScore("C:\\Repos\\AdventOfCode2022\\day2\\day2_ex.txt")
 	fmt.Println(smallResult)
-	result := CalulateScore(GetInput("C:\\Repos\\AdventOfCode2022\\day2\\day2.txt"))
+	result := CalulateScore("C:\\Repos\\AdventOfCode2022\\day2\\day2.txt")
 	fmt.Println(result)
 }
 
@@ -16,14 +16,34 @@ func Part2() {
 
 }
 
-func CalulateScore(input []int) int {
-	// resultList := []int{}
-	// length := len(input)
-	sum := 0
-	return sum
-}
-
-func GetInput(filePath string) []int {
-
-	return nil
+func CalulateScore(filePath string) int {
+	lines := tools.ReadFile(filePath)
+	length := len(lines)
+	total := 0
+	for i := 0; i < length; i++ {
+		line := lines[i]
+		result := 0
+		switch line {
+		case "A X":
+			result = 4
+		case "A Y":
+			result = 8
+		case "A Z":
+			result = 3
+		case "B X":
+			result = 1
+		case "B Y":
+			result = 5
+		case "B Z":
+			result = 9
+		case "C X":
+			result = 7
+		case "C Y":
+			result = 2
+		case "C Z":
+			result = 6
+		}
+		total = total + result
+	}
+	return total
 }
