ceeafe230a9e24edfdfe1eba4ae0f3bec2a2f7ad
Geuskens Ben <ben.geuskens@cegeka.com>
2022-12-05 16:52:21 +0100

    Let's GO

---
+++ day1/day1.go
@@ -0,0 +1,13 @@
+package day1
+
+import (
+	"fmt"
+)
+
+func Part2() {
+
+}
+
+func Part1() {
+	fmt.Println("Hello world")
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"AdventOfCode2022/day1"
+)
+
+func main() {
+	Day1()
+}
+
+func Day1() {
+	day1.Part1()
+}
