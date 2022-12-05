03f1a6469ad3c568d50d53d5ab013e083a981166
Geuskens Ben <ben.geuskens@cegeka.com>
2022-12-05 17:18:11 +0100

    lazy first try

--- day1/day1.go
+++ day1/day1.go
@@ -9,5 +9,32 @@ func Part2() {
 }
 
 func Part1() {
-	fmt.Println("Hello world")
+	smallResult := CalculateResult(SmallInput())
+	fmt.Println(smallResult)
+}
+
+func CalculateResult(input []int) int {
+	resultList := []int{}
+	length := len(input)
+	sum := 0
+	for i := 0; i < length-1; i++ {
+		calories := input[i]
+		sum = sum + calories
+		if calories == 0 {
+			resultList = append(resultList, sum)
+			sum = 0
+		}
+	}
+	var largerNumber, temp int
+	for _, element := range resultList {
+		if element > temp {
+			temp = element
+			largerNumber = temp
+		}
+	}
+	return largerNumber
+}
+
+func SmallInput() []int {
+	return []int{1000, 2000, 3000, 0, 4000, 0, 5000, 6000, 0, 7000, 8000, 9000, 0, 10000}
 }
